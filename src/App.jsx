987184fa53c72f0bde2b1e3a6261d957b4d8a20e
JNHNG0205 <heng.teh0205@gmail.com>
2024-10-11 09:33:07 +0800

    some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import React, { useContext } from 'react'
-import Sidebar from './components/Sidebar'
-import Headerbar from './components/Headerbar'
-import Musicplayer from './components/Musicplayer'
-import Display from './components/Display'
-import { PlayerContext } from './context/PlayerContext'
+import React, { useContext } from "react";
+import Sidebar from "./components/Sidebar";
+import Headerbar from "./components/Headerbar";
+import Musicplayer from "./components/Musicplayer";
+import Display from "./components/Display";
+import { PlayerContext } from "./context/PlayerContext";
 
 const App = () => {
-
-  const {audioRef,track} = useContext(PlayerContext) 
+  const { audioRef, track } = useContext(PlayerContext);
 
   return (
-    <div className='h-screen bg-black'>
-      <Headerbar/>
-      <div className = 'h-[80%] flex'>
-        <Sidebar/>
-        <Display/>
+    <div className="h-screen bg-black">
+      <Headerbar />
+      <div className="h-[80%] flex">
+        <Sidebar />
+        <Display />
       </div>
-      <Musicplayer/>
-      <audio ref={audioRef} src={track.file} preload='auto'></audio>
+      <Musicplayer />
+      <audio ref={audioRef} src={track.file} preload="auto"></audio>
     </div>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
